64f9b895da37be302da022727159470ab525b940
devbearu <mail@devbear.ru>
2021-03-13 14:15:05 +0300

    improving register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -16,6 +16,7 @@
 								class="form-control form-control-lg"
 								type="text"
 								placeholder="Username"
+								v-model="username"
 							/>
 						</fieldset>
 						<fieldset class="form-group">
@@ -23,6 +24,7 @@
 								class="form-control form-control-lg"
 								type="text"
 								placeholder="Email"
+								v-model="email"
 							/>
 						</fieldset>
 						<fieldset class="form-group">
@@ -30,6 +32,7 @@
 								class="form-control form-control-lg"
 								type="password"
 								placeholder="Password"
+								v-model="password"
 							/>
 						</fieldset>
 						<button
@@ -48,6 +51,13 @@
 <script>
 export default {
 	name: 'McvRegister',
+	data() {
+		return {
+			email: '',
+			password: '',
+			username: ''
+		}
+	},
 	computed: {
 		isSubmitting() {
 			return this.$store.state.auth.isSubmitting
@@ -55,9 +65,16 @@ export default {
 	},
 	methods: {
 		onSubmit() {
-			this.$store.dispatch('register').then(user => {
-				console.log('successfully register user', user)
-			})
+			this.$store
+				.dispatch('register', {
+					email: this.email,
+					username: this.username,
+					password: this.password
+				})
+				.then(user => {
+					console.log('successfully register user', user)
+					this.$router.push({name: 'home'})
+				})
 		}
 	}
 }
