7ca366a251284d19bf0ae094d0913386e696ee97
devbearu <mail@devbear.ru>
2021-03-13 15:01:27 +0300

    create validation errors

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -9,7 +9,10 @@
 							Need an account?
 						</router-link>
 					</p>
-					VALIDATION ERRORS
+					<mcv-validation-errors
+						v-if="validationErrors"
+						:validation-errors="validationErrors"
+					/>
 					<form @submit.prevent="onSubmit">
 						<fieldset class="form-group">
 							<input
@@ -49,8 +52,12 @@
 </template>
 
 <script>
+import McvValidationErrors from '@/components/ValidationErrors'
 export default {
 	name: 'McvRegister',
+	components: {
+		McvValidationErrors
+	},
 	data() {
 		return {
 			email: '',
@@ -61,6 +68,9 @@ export default {
 	computed: {
 		isSubmitting() {
 			return this.$store.state.auth.isSubmitting
+		},
+		validationErrors() {
+			return this.$store.state.auth.validationErrors
 		}
 	},
 	methods: {
