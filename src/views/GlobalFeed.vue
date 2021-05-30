be988010fce2259bbfe237c3f040137848f42f30
devbearu <mail@devbear.ru>
2021-05-30 21:45:11 +0300

    create feed toggler

--- src/views/GlobalFeed.vue
+++ src/views/GlobalFeed.vue
@@ -1,9 +1,10 @@
 <template>
 	<div class="home-page">
-	<mcv-banner />
+		<mcv-banner />
 		<div class="container page">
 			<div class="row">
 				<div class="col-md-9">
+					<mcv-feed-toggler />
 					<mcv-feed :api-url="apiUrl"></mcv-feed>
 				</div>
 				<div class="col-md-3">
@@ -18,13 +19,15 @@
 import McvFeed from '@/components/Feed'
 import McvPopularTags from '@/components/PopularTags'
 import McvBanner from '@/components/Banner'
+import McvFeedToggler from '@/components/FeedToggler'
 
 export default {
 	name: 'McvGlobalFeed',
 	components: {
 		McvFeed,
 		McvPopularTags,
-		McvBanner
+		McvBanner,
+		McvFeedToggler
 	},
 	data() {
 		return {
